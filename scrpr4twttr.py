3475be77eb45eb56ebbe4b77e474f6525c4dbaee
Artaxexes J. Ferreira <artaxexes@gmail.com>
2016-10-07 15:14:51 -0300

    search and storage

--- scrpr4twttr.py
+++ scrpr4twttr.py
@@ -7,30 +7,32 @@ import pymongo
 
 # mongodb connection
 mongo_conf = {
-	"host" : "localhost",
-	"port" : 27017
+	'host' : 'localhost',
+	'port' : 27017
 } 
-mongo = pymongo.MongoClient(mongo_conf["host"], mongo_conf["port"])
+mongo = pymongo.MongoClient(mongo_conf['host'], mongo_conf['port'])
 mongo_db = mongo.test
-mongo_collection = mongo_db.tweets2
+mongo_collection = mongo_db.tweets
 
-# twitter connection
+# twitter connection (https://apps.twitter.com/)
 oauth = twitter.OAuth(
-	consumer_key = '',
-	consumer_secret = '',
-	token = '',
-	token_secret = ''
+	consumer_key = '',	# consumer key (api key) goes here
+	consumer_secret = '',	# consumer secret (api secret) goes here
+	token = '',		# access token goes here
+	token_secret = ''	# access token secret goes here
 )
-twttr = twitter.Twitter(auth = oauth)
-twttr_type = "mixed"
-twttr_qnt = 100
+tt = twitter.Twitter(auth = oauth)
+tt_query = '#rio2016'
+tt_type = 'mixed'
+tt_qnt = 100
 
 # search
-print "Searching..."
-twts = twttr.search.tweets(q = "#rio2016", result_type = twttr_type, count = twttr_qnt)
+print 'Searching...',
+tts = tt.search.tweets(q = tt_query, result_type = tt_type, count = tt_qnt)
 
 # storage
-mongo_collection.insert_many(twts)
+mongo_collection.insert_many(tts['statuses'])
 
 # log
-print "Search of %d %s tweets completed in %.3f seconds" % (twttr_qnt, twttr_type, tweets["search_metadata"]["completed_in"])
+print '[done]'
+print 'Search of %d "%s" %s tweets completed in %.3f seconds' % (tt_qnt, tt_query, tt_type, tts['search_metadata']['completed_in'])
