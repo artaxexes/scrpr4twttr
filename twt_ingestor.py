217f335247798e9afa57963bac053a713aca9da7
Artaxexes J. Ferreira <artaxexes@gmail.com>
2016-10-14 18:49:38 -0300

    exception handler added, more verbose

--- twt_ingestor.py
+++ twt_ingestor.py
@@ -1,38 +1,54 @@
 #!/usr/bin/python
 
+import sys
 import twitter
 import pymongo
 
 
 
-# mongodb connection
-mongo_conf = {
-	'host' : 'localhost',
-	'port' : 27017
-} 
-mongo = pymongo.MongoClient(mongo_conf['host'], mongo_conf['port'])
-mongo_db = mongo.test
-mongo_collection = mongo_db.tweets
+# config mongodb connection: mongodb url, port, database and collection names
+mongo_conf = ('localhost', 27017, 'tweets', 'test')
+mongo = pymongo.MongoClient(mongo_conf[0], mongo_conf[1])
+mongo_db = mongo[mongo_conf[2]]
+mongo_collection = mongo_db[mongo_conf[3]]
 
-# twitter connection (https://apps.twitter.com/)
+
+# config twitter connection: consumer key, consumer secret, access token, access token secret
 oauth = twitter.OAuth(
-	consumer_key = '', # consumer key (api key) goes here
-	consumer_secret = '', # consumer secret (api secret) goes here
-	token = '', # access token goes here
-	token_secret = '' # access token secret goes here
+  consumer_key = '',
+  consumer_secret = '',
+  token = '',
+  token_secret = ''
 )
-tt = twitter.Twitter(auth = oauth)
-tt_query = '#rio2016'
-tt_type = 'mixed'
-tt_qnt = 10
 
-# search
-print 'Searching...',
-tts = tt.search.tweets(q = tt_query, result_type = tt_type, count = tt_qnt)
 
-# storage
-mongo_collection.insert_many(tts['statuses'])
+# twitter authentication
+print 'connecting... ',
+try:
+  tt = twitter.Twitter(auth = oauth)
+  user = tt.account.settings()
+  print '\t[done] connected as', user['screen_name']
+except:
+  print '\t[error]', sys.exc_info()[1]
+  raise
 
-# log
-print '[done]'
-print 'Search of %d "%s" %s tweets completed in %.3f seconds' % (tt_qnt, tt_query, tt_type, tts['search_metadata']['completed_in'])
+
+# tweets search
+print 'searching... ',
+tt_args = ('#temer', 'recent', 100, 'pt')
+tts = tt.search.tweets(q = tt_args[0], result_type = tt_args[1], count = tt_args[2], lang = tt_args[3])
+print '\t[done] search of %d "%s" %s tweets completed in %.3f seconds' % (tt_args[2], tt_args[0], tt_args[1], tts['search_metadata']['completed_in'])
+
+
+# tweets storage
+print 'storing... ',
+try:
+  mongo_collection.insert_many(tts['statuses'])
+  print '\t[done] %d tweets stored in %s' % (mongo_collection.count(), mongo_collection.full_name)
+except:
+  print '\t[error]', sys.exc_info()[1]
+  raise
+finally:
+  mongo.close()
+
+print 'finished'
