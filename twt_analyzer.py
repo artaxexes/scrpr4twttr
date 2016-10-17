0d7f9fd9c670b9910171bfcd932e6437f8c37914
Artaxexes J. Ferreira <artaxexes@gmail.com>
2016-10-17 18:53:29 -0200

    aggregate and mapreduce in mongodb

--- twt_analyzer.py
+++ twt_analyzer.py
@@ -1,32 +1,50 @@
 #!/usr/bin/python
 
-import json
+import sys
+import pymongo
+from bson.code import Code
+
+# config mongodb connection: mongodb url, port, database and collection names
+mongo_conf = ('localhost', 27017, 'twitter', 'tweets')
+client = pymongo.MongoClient(mongo_conf[0], mongo_conf[1])
+db = client[mongo_conf[2]]
+collection = db[mongo_conf[3]]
 
 
-
-# load json file with tweets
-tts = json.loads(open('tweets.json').read())
-
-
-# filter(function, iterable)
-# construct a list from those elements of iterable for which function returns true
-was_rtt = filter(lambda tt: tt['retweeted'], tts["statuses"])
-
-
-# map(function, iterable, ...)
-# apply function to every item of iterable and return a list of the results
-was_rtt_user = map(lambda tt: (tt['user'], tt['retweet_count']), was_rtt)
-
-
-# get sum(rtt) by user
-users = {}
-for key, value in was_rtt_user:
-  users[key] = users.get(key, 0) + value
-
-
-# sort the result: more rtt first
-users_sort = sorted(users, key = users.__getitem__, reverse = True)
+# aggregate
+print 'doing aggregate...',
+try:
+  collection.aggregate(
+    [
+      {"$match": {"retweet_count":{"$gt":1}}},
+      {"$group": {"_id":"$user.screen_name","retweeted":{"$sum":"$retweet_count"}}},
+      #{"$sort": {"retweeted":-1}},
+      {"$out":"aggregate"}
+    ]
+  )
+  print '\t[done] aggregate result:', db['aggregate'].count(), 'users'
+  print '\ttop 5 retweeted users'
+  for user in db['aggregate'].find(limit = 5).sort([('retweeted', pymongo.DESCENDING)]):
+    print '\t *', user['_id'], 'retweeted', user['retweeted'], 'times'
+except:
+  print '\t[error]', sys.exc_info()[1]
+  raise
 
 
-# print the top 3 rtt users
-print map(lambda x: x.encode('ascii'), users_sort[:3])
+# map reduce
+print '\ndoing map reduce...',
+try:
+  mapper = Code('function(){ emit(this.user.screen_name, this.retweet_count); }')
+  reducer = Code('function(key, values){ return Array.sum(values); }')
+  collection.map_reduce(mapper, reducer, 'mapreduce', query = {'retweet_count':{'$gt':1}})
+  print '\t[done] map reduce result:', db['mapreduce'].count(), 'users'
+  print '\ttop 5 retweeted users'
+  for user in db['mapreduce'].find(limit = 5).sort([('value', pymongo.DESCENDING)]):
+    print '\t *', user['_id'], 'retweeted', int(user['value']), 'times'
+except:
+  print '\t[error]', sys.exc_info()[1]
+  raise
+finally:
+  client.close
+
+print 'finished'
