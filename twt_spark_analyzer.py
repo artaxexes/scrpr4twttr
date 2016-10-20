f189611ccc469520e65ffd52f7ce0a8037abec21
Artaxexes J. Ferreira <artaxexes@gmail.com>
2016-10-20 19:03:39 -0200

    script for spark

---
+++ twt_spark_analyzer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+print 'calm down'
+#rtt = df.filter(df['retweet_count'] >= 1)
+#df = sqlContext.read.format('com.mongodb.spark.sql.DefaultSource').load()
