4d4454eb48fdc7ee65a64ab1bbe83808f03556c1
Artaxexes J. Ferreira <artaxexes@gmail.com>
2016-10-24 19:04:45 -0200

    spark-shell start command line

--- twt_spark_analyzer.scala
+++ twt_spark_analyzer.scala
@@ -1,2 +1,9 @@
-val tts = MongoSpark.load(sc)
-val rtts = rdd.filter(doc => doc.getInteger("retweet_count") >= 1)
+/*
+ *
+ * ./spark-shell --conf "spark.mongodb.input.uri=mongodb://127.0.0.1/twitter.tweets?readPreference=primaryPreferred" --conf "spark.mongodb.output.uri=mongodb://127.0.0.1/twitter.tweets" --packages org.mongodb.spark:mongo-spark-connector_2.11:2.0.0-rc0
+ *
+ * import com.mongodb.spark._
+ * val tts = MongoSpark.load(sc)
+ * val rtts = rdd.filter(doc => doc.getInteger("retweet_count") >= 1)
+ *
+ */
